a983355eecd5ca5c3df2bc74199b10f8ae309965
Rochelle1453 <hoperochelleandales@gmail.com>
2026-03-26 18:03:52 +0800

    Cleaned: Removed unnecessary attributes, added proper id for likes.

--- types/project.ts
+++ types/project.ts
@@ -1,11 +1,11 @@
 export type Project = {
+  id: string;
   title: string;
   category?: string;
   description: string;
   tags: string[];
   image: string;
   links: {
-    demo: string;
     github: string;
   };
 };
